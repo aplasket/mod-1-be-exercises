6e9ef7053219456241ee7270223445d0466ce745
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-03 12:47:06 -0400

    define add room and sort rooms method

--- lessons/reaching_through_objects/exercise_1/lib/apartment.rb
+++ lessons/reaching_through_objects/exercise_1/lib/apartment.rb
@@ -1,8 +1,9 @@
 class Apartment
-  attr_reader :rented
+  attr_reader :rented, :rooms
 
   def initialize
     @rented = false
+    @rooms = []
   end
 
   def is_rented?
@@ -12,4 +13,16 @@ class Apartment
   def rent
     @rented = true
   end
+
+  def add_room(room_type)
+    if @rooms.count < 4
+      @rooms << room_type
+    end
+  end
+
+  def list_rooms_by_name_alphabetically
+    sorted_rooms = (@rooms.map {|room| room.name}).sort
+  end
+
+
 end
--- lessons/reaching_through_objects/exercise_1/spec/apartment_spec.rb
+++ lessons/reaching_through_objects/exercise_1/spec/apartment_spec.rb
@@ -23,4 +23,26 @@ RSpec.describe Apartment do
       expect(@apartment.is_rented?).to eq(true)
     end
   end
+
+  before(:each) do
+    @bathroom = Room.new("bathroom")
+    @apartment.add_room(@bathroom)
+    @apartment.add_room(Room.new("laundry"))
+    @apartment.add_room(Room.new("kitchen"))
+    @apartment.add_room(Room.new("bedroom"))
+  end
+
+  describe '#adds room' do
+    it 'can create rooms and adds to array' do
+      expect(@bathroom).to be_a(Room)
+      expect(@apartment.rooms).to be_an(Array)
+      expect(@apartment.rooms.first).to be_a(Room)
+    end
+
+    it 'sorts rooms alphabetically' do
+      expected_results = ["bathroom", "bedroom", "kitchen", "laundry"]
+      expect(@apartment.list_rooms_by_name_alphabetically). to eq(expected_results)
+    end
+    
+  end
 end
