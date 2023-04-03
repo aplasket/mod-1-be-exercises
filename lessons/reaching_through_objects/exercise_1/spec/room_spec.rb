53eb74a483c56b9ef8e3d816ff2ceb7ad8ca4aae
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-03 12:06:14 -0400

    add before(:each) to instantiate a few new rooms

--- lessons/reaching_through_objects/exercise_1/spec/room_spec.rb
+++ lessons/reaching_through_objects/exercise_1/spec/room_spec.rb
@@ -2,17 +2,20 @@ require 'pry'
 require './lib/room.rb'
 
 RSpec.describe Room do
+  before(:each) do
+    @bathroom = Room.new("bathroom")
+    @bedroom = Room.new("bedroom")
+    @kitchen = Room.new("kitchen")
+  end
+
    describe 'initialize' do
     it 'can initialize' do
-      bathroom = Room.new("bathroom")
-
-      expect(bathroom).to be_a(Room)
+      expect(@bathroom).to be_a(Room)
     end
 
     it 'has readable attributes' do
-      bathroom = Room.new("bathroom")
-      
-      expect(bathroom.name).to eq("bathroom")
+      expect(@bathroom.name).to eq("bathroom")
+      expect(@bedroom.name).to eq("bedroom")
     end
   end
 end
