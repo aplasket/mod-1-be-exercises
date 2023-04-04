8fa9d9ab253cd56c4f26d18b014c07973c6c7895
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-04 11:17:48 -0400

    add in answers for exercises

--- lessons/nested_collections/Examples/warm_up.rb
+++ lessons/nested_collections/Examples/warm_up.rb
@@ -1,12 +1,19 @@
 animals = {"dogs" => 3, "cats" => 5, "iguanas" => 2}
 
 # 1. return the value of dogs
+p animals["dogs"]
 # 2. add 3 parakeets
+p animals["parakeets"] = 3
 # 3. increase the amount of cats by 2
+p animals["cats"] += 2
 
 pet_names = ["Fela", "Spot", "Patch", "Willy"]
 
 # 1. add “Claude” to the end of the array
+p pet_names.insert(4, "Claude")
 # 2. access the first name in the list
+p pet_names.first
 # 3. access the last name in the list
+p pet_names.last
 # 4. remove “Fela” from the list
+p pet_names.shift
