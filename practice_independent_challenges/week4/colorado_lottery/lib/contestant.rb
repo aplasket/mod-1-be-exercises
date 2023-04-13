ef7a94743c537c9dfc2821e6b10863b5bc4d7c58
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-13 15:48:09 -0400

    initialize contestant class and add attributes

---
+++ practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
@@ -0,0 +1,13 @@
+class Contestant
+  attr_reader :full_name,
+              :age,
+              :state_of_residence,
+              :spending_money
+
+  def initialize(contestant_details)
+    @full_name = contestant_details[:first_name] + " " + contestant_details[:last_name]
+    @age = contestant_details[:age]
+    @state_of_residence = contestant_details[:state_of_residence]
+    @spending_money = contestant_details[:spending_money]
+  end
+end
--- practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/contestant_spec.rb
@@ -1,9 +1,24 @@
 require "spec_helper"
 
-Rspec.describe Contestant do
+RSpec.describe Contestant do
+  before(:each) do
+    @alexander = Contestant.new({ first_name: 'Alexander',
+                                  last_name: 'Aigiades',
+                                  age: 28,
+                                  state_of_residence: 'CO',
+                                  spending_money: 10})
+  end
   describe "#initialize" do
     it "exists" do
-      expect()
+      expect(@alexander).to be_a(Contestant)
     end
+
+    it "has attributes" do
+      expect(@alexander.full_name).to eq("Alexander Aigiades")
+      expect(@alexander.age).to eq(28)
+      expect(@alexander.state_of_residence).to eq("CO")
+      expect(@alexander.spending_money).to eq(10)
+    end
+    
   end
 end
