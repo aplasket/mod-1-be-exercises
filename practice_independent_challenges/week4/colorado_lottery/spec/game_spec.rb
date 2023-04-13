3846f830446a232db7c5658bcda6ed6c21016859
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-13 15:39:13 -0400

    add attributes to game file

--- practice_independent_challenges/week4/colorado_lottery/spec/game_spec.rb
+++ practice_independent_challenges/week4/colorado_lottery/spec/game_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe Game do
 
     it "has attributes" do
       expect(@mega_millions.name).to eq("Mega Millions")
+      expect(@mega_millions.cost).to eq(5)
+    end
+  end
+  
+  describe "#national drawing?" do
+    it "returns a boolean based on national_drawing attribute" do
+      expect(@mega_millions.national_drawing?).to eq(true)
+      expect(@pick_4.national_drawing?).to eq(false)
     end
   end
 end
