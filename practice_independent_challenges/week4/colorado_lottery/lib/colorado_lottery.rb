c8deeec7dea722eff74b1259b9ed5ec1cb89f5d5
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-13 20:29:22 -0400

    add register method and tests

--- practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
@@ -8,4 +8,15 @@ class ColoradoLottery
     @winners = []
     @current_contestants = {}
   end
+
+  def interested_and_18?(name, game)
+    return false unless name.game_interests.include?(game.name) && name.age >= 18
+    true
+  end
+
+  def can_register?(name, game)
+    return false unless interested_and_18?(name, game) &&
+                        (!name.out_of_state? || game.national_drawing?)
+    true
+  end
 end
