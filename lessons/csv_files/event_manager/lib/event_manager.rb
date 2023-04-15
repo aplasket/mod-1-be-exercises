a3bdb5812703ef3f62bfcab0f5bbf7289c10af5c
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-15 18:46:16 -0400

    play around with data

--- lessons/csv_files/event_manager/lib/event_manager.rb
+++ lessons/csv_files/event_manager/lib/event_manager.rb
@@ -1,6 +1,97 @@
 # ./lib/event_manager.rb
 require "csv"
+
 puts "EventManager initialized."
 
-contents = File.read "event_attendees.csv"
-puts contents
+# contents = File.read "event_attendees.csv"
+# puts contents
+
+# puts File.exist? "event_attendees.csv"
+
+# lines = File.readlines "event_attendees.csv"
+# lines.each do |line|
+#   puts line
+# end
+
+# lines = File.readlines "event_attendees.csv"
+# row_index = 0
+# lines.each do |line|
+#   row_index = row_index + 1
+#   next if row_index == 1
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+
+# lines = File.readlines "event_attendees.csv"
+# lines.each_with_index do |line,index|
+#   next if index == 0
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+# contents = CSV.open "event_attendees.csv", headers: true
+# contents.each do |row|
+#   name = row[2]
+#   puts name
+# end
+
+#accessing first name
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = row[:first_name]
+#   puts name
+# end
+
+#accessing zip code:
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = row[:zipcode]
+#   puts "#{name} #{zipcode}"
+# end
+
+#fixing nil and/or bad zip code formatting
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = row[:zipcode]
+
+#   if zipcode.nil?
+#     zipcode = "00000"
+#   elsif zipcode.length < 5
+#     zipcode = zipcode.rjust 5, "0"
+#   elsif zipcode.length > 5
+#     zipcode = zipcode[0..4]
+#   end
+
+#   puts "#{name} #{zipcode}"
+# end
+
+## refactored:
+# def clean_zipcode(zipcode)
+#   if zipcode.nil?
+#     "00000"
+#   elsif zipcode.length < 5
+#     zipcode.rjust(5,"0")
+#   elsif zipcode.length > 5
+#     zipcode[0..4]
+#   else
+#     zipcode
+#   end
+# end
+
+#refactored clean zipcode:
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  clean_zipcode(zipcode)
+  puts "#{name} #{zipcode}"
+end
