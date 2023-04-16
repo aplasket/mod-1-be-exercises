ae8467e3631dc6e34f4b3e3dd1af869b566ea926
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-16 13:33:10 -0400

    initialize contestant class

---
+++ practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
@@ -0,0 +1,24 @@
+class Contestant
+  attr_reader :full_name,
+              :age,
+              :state_of_residence,
+              :spending_money,
+              :game_interests
+
+  def initialize(contestant_info)
+    @full_name = "#{contestant_info[:first_name]} #{contestant_info[:last_name]}"
+    @age = contestant_info[:age]
+    @state_of_residence = contestant_info[:state_of_residence]
+    @spending_money = contestant_info[:spending_money]
+    @game_interests = []
+  end
+
+  def out_of_state?
+    return true unless state_of_residence == "CO"
+    false
+  end
+
+  def add_game_interest(game_name)
+    @game_interests << game_name
+  end
+end
